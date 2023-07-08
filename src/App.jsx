e0c9dd046486d17d042b6acf5c850182a5762e7f
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-09 01:48:13 +0530

    Added map library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Map
+      mapboxAccessToken={import.meta.env.VITE_APP_MAPBOX_TOKEN}
+      initialViewState={{
+        longitude: -95.665,
+        latitude: 37.6,
+        zoom: 3,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/abhiramkrishna8921/cljufl47a001z01pj96ax2sne"
+    />
+  );
 }
 
-export default App
+export default App;
