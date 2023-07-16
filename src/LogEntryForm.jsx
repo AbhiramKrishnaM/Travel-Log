a976d90d8a1d58f148aad8d7be69e7841e571d6a
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-16 19:16:39 +0530

    fixed issue

--- src/LogEntryForm.jsx
+++ src/LogEntryForm.jsx
@@ -1,16 +1,28 @@
+import { useForm } from "react-hook-form";
+
 function LogEntryForm() {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <form className="entry-form">
-      <label for="title">Title</label>
-      <input name="title" required />
-      <label for="comments">Comments</label>
-      <textarea name="comments" row={3}></textarea>
-      <label for="description">Description</label>
-      <textarea name="description" row={3}></textarea>
-      <label for="image">Image </label>
-      <input name="image" />
-      <label for="visitdate">Visit Date</label>
-      <input name="visitdate" type="date" required />
+    <form className="entry-form" onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="title">Title</label>
+      <input name="title" required {...register("title")} />
+      <label htmlFor="comments">Comments</label>
+      <textarea name="comments" {...register("comments")} row={3}></textarea>
+      <label htmlFor="description">Description</label>
+      <textarea
+        name="description"
+        {...register("description")}
+        row={3}
+      ></textarea>
+      <label htmlFor="image">Image </label>
+      <input name="image" {...register("image")} />
+      <label htmlFor="visitdate">Visit Date</label>
+      <input name="visitdate" type="date" {...register("visitDate")} required />
       <button>Create Entry</button>
     </form>
   );
